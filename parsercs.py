2459217a2beee2011cadc99a0fc20c3f217f528e
Mark Moraes <moraes@computer.org>
2020-02-19 19:02:07 -0500

    version that works ok on some RCS, but clustering still poor

--- parsercs.py
+++ parsercs.py
@@ -10,13 +10,21 @@ from datetime import datetime
 from pprint import pprint
 from collections import defaultdict
 
-def rlogparse(fnames, debug = 1, timegranularity = 360.):
+fhead = "head"
+frfile = "rcs"
+fwfile = "file"
+frevs = "revs"
+fdesc = "desc"
+fdate = "date"
+fdt = "dt"
+fauth = "author"
+
+descskip = set(("initial revision",))
+
+def rlogparse(fnames, debug = 0):
     """Run rlog on fnames and parse the output into a list of per-file
        RCS metadata, with a dict of metadata for each file, each containing
-       sub-dict of revision data.  Also returns a dict of revisions by date,
-       with key being the dates rounded to nearest timegranularity seconds,
-       values are list of revisions tuples, each tuple contains actual date
-       working filename, revision#, author and description.
+       sub-dict of revision data.
     """
     cmd = ["/usr/bin/rlog"] + fnames
     if debug>1: print(cmd)
@@ -29,18 +37,9 @@ def rlogparse(fnames, debug = 1, timegranularity = 360.):
     krevdate = "date:"
     kauth = "author:"
     kblockend = "----------------------------"
-    fhead = "head"
-    frfile = "rcs"
-    fwfile = "file"
-    frevs = "revs"
-    fdesc = "desc"
-    fdate = "date"
-    fdt = "dt"
-    fauth = "author"
     kend = "============================================================================="
 
-    rcsdata = []
-    rcsbydate = defaultdict(list)
+    rcsmeta = []
     with subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE) as p:
         newfile = True
         nr = nf = 0
@@ -54,17 +53,13 @@ def rlogparse(fnames, debug = 1, timegranularity = 360.):
             if debug>1: print("read", repr(s))
             if s == kend:
                 d = {frfile:vrfile, fwfile:vwfile, fhead:vhead, fdesc:vdesc, frevs:vrevs}
-                rcsdata.append(d)
+                rcsmeta.append(d)
                 newfile = True # clears the state above on next line
             elif s == kblockend and inblock != None:
                 if inblock == fdesc:
                     if debug>1: print("desc end:", vdesc)
                 else:
                     if debug>1: print("rev end:", vrevs[inblock])
-                    t = vrevs[inblock][fdt].timestamp()
-                    hr = int(t/timegranularity + 0.5)*timegranularity
-                    hrstr = datetime.fromtimestamp(hr)
-                    rcsbydate[hrstr].append((vrevs[inblock][fdate],vwfile,inblock,vrevs[inblock][fauth],vrevs[inblock][fdesc]))
                 inblock = None
             elif inblock != None:
                 if inblock == fdesc:
@@ -107,17 +102,51 @@ def rlogparse(fnames, debug = 1, timegranularity = 360.):
                 assert inblock not in vrevs, repr(inblock)
                 vrevs[inblock] = {fdesc:"", fauth:sauth, fdate:sdate, fdt:dt}
     if debug: print("read", nf, "files,", nr, "lines")
-    return rcsdata, rcsbydate
+    return rcsmeta
+
+def rcscluster(rcsmeta, timegranularity, debug = 0):
+    """Also returns a dict of revisions by date,
+       with key being the dates rounded to nearest timegranularity seconds,
+       values are list of revisions tuples, each tuple contains actual date
+       working filename, revision#, author and description.
+     """
+    revsbydate = defaultdict(list)
+    files = defaultdict(dict)
+    for f in rcsmeta:
+        vrfile = f[frfile]
+        vwfile = f[fwfile]
+        vrevs = f[frevs]
+        for r in vrevs:
+            rinfo = vrevs[r]
+            t = rinfo[fdt].timestamp()
+            hr = int(t/timegranularity + 0.5)*timegranularity
+            hrstr = datetime.fromtimestamp(hr).isoformat()
+            rkey = (hrstr,rinfo[fauth])
+            if vrfile in files[rkey]:
+                raise RuntimeError("file "+vrfile+" already in "+repr(rkey)+" : "+repr(files[rkey][vrfile])+"\n"+repr(rinfo)+"\nMaybe try lowering GRANULARITY")
+            files[rkey][vrfile] = rinfo
+            revsbydate[rkey].append((rinfo[fdate],vrfile,r,vwfile,rinfo[fdesc]))
+    rcscommits = {}
+    for c in sorted(revsbydate):
+        commit = [[], []]
+        revs = revsbydate[c]
+        for r in sorted(revs):
+            vdesc = r[-1]
+            if vdesc.strip().lower() not in descskip and vdesc not in commit[0]:
+                commit[0].append(vdesc)
+            commit[1].append(r[:-1])
+        rcscommits[c] = commit
+    return rcscommits
 
 def main():
     if len(sys.argv) == 1 or sys.argv[1].startswith('-'):
         sys.exit("Usage: "+sys.argv[0]+" RCSFILES");
-    rmeta, rdate = rlogparse(sys.argv[1:], debug=1)
+    rmeta = rlogparse(sys.argv[1:])
     if 0:
         pprint(rmeta, compact=True)
-    if 1:
-        for d in sorted(rdate):
-            print(d, rdate[d])
+    rdate = rcscluster(rmeta, float(os.getenv("GRANULARITY", "600")))
+    for d in sorted(rdate):
+        pprint((d, rdate[d]), width=os.get_terminal_size(0).columns)
     return 0
     
 if __name__ == '__main__':
